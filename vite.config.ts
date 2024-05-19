d6dbdb8799b3094b3476ad49ca77481332f3dba2
lunarW1TCH <lunarw1tch@proton.me>
2024-05-19 08:55:17 +0200

    fix: changes tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,12 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
     Pages({
       dirs: [{ dir: 'src/pages', baseRoute: '' }],
     }),
     tsconfigPaths(),
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
   ],
 });
